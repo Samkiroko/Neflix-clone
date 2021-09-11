86e0969a3eadbb6810be8c9bf04455facee41e8d
Samkiroko <kirokodev@gmail.com>
2021-09-11 12:36:45 +0300

    Implementing auth with firebase

--- .history/src/App_20210911123052.js
+++ .history/src/App_20210911123052.js
@@ -8,14 +8,13 @@ import { auth } from './firebase'
 function App() {
   const user = null
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
-      if (userAuth) {
-        console.log(userAuth)
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user)
       } else {
         console.log('no user')
       }
     })
-    return unsubscribe
   }, [])
   return (
     <div className='app'>
--- .history/src/App_20210911123132.js
+++ .history/src/App_20210911123132.js
@@ -8,14 +8,13 @@ import { auth } from './firebase'
 function App() {
   const user = null
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+    auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         console.log(userAuth)
       } else {
         console.log('no user')
       }
     })
-    return unsubscribe
   }, [])
   return (
     <div className='app'>
